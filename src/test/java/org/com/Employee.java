96fd19c3f5f68e0069b205d84c07e5334dd827ee
SUDHAGAR98 <sudhagar203982gmail.com>
2023-04-03 18:32:48 +0530

    first

---
+++ src/test/java/org/com/Employee.java
@@ -0,0 +1,18 @@
+package org.com;
+
+public class Employee {
+	private void method1() {
+     System.out.println("sudha");
+	}
+	private void method2() {
+	     System.out.println("subesh");
+		}
+	private void method3() {
+	     System.out.println("Bharathi");
+		}
+
+	
+	
+	
+
+}
